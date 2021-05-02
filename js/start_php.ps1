1be59fa668be53bb9b2be4409f544c0aabd94e68
zaopuppy <zhaoyi.zero@gmail.com>
2021-05-02 16:47:49 +0800

    add js

---
+++ js/start_php.ps1
@@ -0,0 +1,4 @@
+$php = "C:\zhaoyi\sdk\php\php-8.0.5-nts-Win32-vs16-x64\php.exe"
+
+# & $php --help
+& $php -S 127.0.0.1:1984
---
+++ js/worker/worker.js
@@ -0,0 +1,5 @@
+
+self.onmessage = function (msg) {
+    // console.info('messge: ' + msg.data.type);
+    console.info('messge');
+}
