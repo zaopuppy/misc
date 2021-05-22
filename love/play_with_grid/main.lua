17993626236b5f9b0547c8e6e5250852067bdb8e
zaopuppy <zhaoyi.zero@gmail.com>
2021-05-22 13:24:09 +0800

    play with grids

--- love/play_with_grid/main.lua
+++ love/play_with_grid/main.lua
@@ -12,8 +12,8 @@ g_rows = 0
 g_scr_width = 800
 g_scr_height = 600
 
-g_scr_offset_x = 0
-g_scr_offset_y = 0
+g_scr_offset_x = 0 + g_grid_width*3
+g_scr_offset_y = 0 + g_grid_height*3
 
 g_marked_grids = {}
 
@@ -24,6 +24,7 @@ g_mouse_y = 0
 -- Load some default values for our rectangle.
 function love.load()
     update_col_and_row(g_scr_width, g_scr_height)
+    love.window.setTitle("Grids")
     love.window.setMode(g_scr_width, g_scr_height, {resizable=true})
 end
 
@@ -75,6 +76,14 @@ function love.draw()
             g_scr_offset_y + (r*g_grid_height),
             g_grid_width, g_grid_height)
     end
+    -- draw position for makred grids
+    love.graphics.setColor(1, 1, 1)
+    for k, v in pairs(g_marked_grids) do
+        local c, r = v[1], v[2]
+        love.graphics.print(string.format("(%d, %d)", c, r),
+            g_scr_offset_x + (c*g_grid_width),
+            g_scr_offset_y + (r*g_grid_height))
+    end
 end
 
 
@@ -83,19 +92,32 @@ function love.mousemoved(x, y, dx, dy, istouch)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-    x = x + g_scr_offset_x
-    y = y + g_scr_offset_y
+    x = x - g_scr_offset_x
+    y = y - g_scr_offset_y
     local col, row = math.floor(x/g_grid_width), math.floor(y/g_grid_height)
     if button == 1 then
-        table.insert(g_marked_grids, {col, row})
+        -- mark grid
+        local idx = find_idx(g_marked_grids, col, row)
+        if idx < 0 then
+            table.insert(g_marked_grids, {col, row})
+        end
     elseif button == 2 then
-        for k, v in pairs(g_marked_grids) do
-            local c, r = v[1], v[2]
-            if c == col and r == row then
-                table.remove(g_marked_grids, k)
-            end
+        -- unmark grid
+        local idx = find_idx(g_marked_grids, col, row)
+        if idx >= 0 then
+            table.remove(g_marked_grids, idx)
         end
     end
+end
+
+function find_idx(grids, col, row)
+    for k, v in pairs(g_marked_grids) do
+        local c, r = v[1], v[2]
+        if c == col and r == row then
+            return k
+        end
+    end
+    return -1
 end
 
 function love.resize(w, h)
@@ -105,6 +127,14 @@ end
 function love.keypressed(key, scancode, isrepeat)
     if key == 'q' then
         love.event.quit()
+    elseif key == 'w' or key == 'up' then
+        g_scr_offset_y = g_scr_offset_y - math.floor(g_grid_height/2)
+    elseif key == 's' or key == 'down' then
+        g_scr_offset_y = g_scr_offset_y + math.floor(g_grid_height/2)
+    elseif key == 'a' or key == 'left' then
+        g_scr_offset_x = g_scr_offset_x - math.floor(g_grid_width/2)
+    elseif key == 'd' or key == 'right' then
+        g_scr_offset_x = g_scr_offset_x + math.floor(g_grid_width/2)
     end
 end
 
