dd3632fda9fc5fa0c7b61539ef4c2029bb681195
z00302892 <joey.zhaoyi@huawei.com>
2021-05-28 21:34:11 +0800

    light2d

---
+++ sdl2/light2d/main.cc
@@ -0,0 +1,40 @@
+
+#include <cstdio>
+
+#include <Windows.h>
+
+#include <SDL.h>
+
+using namespace std;
+
+
+// int main(int argc, char* argv[]) {
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+    printf("Just a test.\n");
+
+    // SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(0);
+
+    auto window = SDL_CreateWindow("light2d",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600, SDL_WINDOW_RESIZABLE);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+    // auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    auto surface = SDL_GetWindowSurface(window);
+
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
+}
+
