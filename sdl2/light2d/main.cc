85b471ab475f4148c54e0ee387bd371e53b69d7d
z00302892 <joey.zhaoyi@huawei.com>
2021-05-29 16:23:58 +0800

    light2d

--- sdl2/light2d/main.cc
+++ sdl2/light2d/main.cc
@@ -1,5 +1,7 @@
 
-#include <cstdio>
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
 
 #include <Windows.h>
 
@@ -8,14 +10,37 @@
 using namespace std;
 
 
+#define LOG(fmt, ...) printf(fmt ## "\n", __VA_ARGS__)
+// #define LOG(s) OutputDebugStringA(s)
+
+
+void open_console() {
+    AllocConsole();
+
+    FILE *stdin_stream;
+    freopen_s(&stdin_stream, "conin$", "r+t", stdin);
+
+    FILE *stdout_stream;
+    freopen_s(&stdout_stream, "conout$", "w+t", stdout);
+
+    // don't forget to free console
+    // FreeConsole();
+    // fclose(stdin_stream);
+    // fclose(stdout_stream);
+}
+
+
 // int main(int argc, char* argv[]) {
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-    printf("Just a test.\n");
 
-    // SDL_Init(SDL_INIT_VIDEO);
-    SDL_Init(0);
+    open_console();
 
-    auto window = SDL_CreateWindow("light2d",
+    LOG("Just a test.");
+
+    SDL_Init(SDL_INIT_VIDEO);
+    // SDL_Init(0);
+
+    SDL_Window* window = SDL_CreateWindow("light2d",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         800, 600, SDL_WINDOW_RESIZABLE);
 
@@ -23,13 +48,30 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     // auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    auto surface = SDL_GetWindowSurface(window);
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
 
-    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0x0, 0xFF));
 
-    SDL_UpdateWindowSurface(window);
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        if (SDL_PollEvent(&event) == 0) {
+            LOG("bad event");
+            running = false;
+        } else {
+            if (event.type = SDL_QUIT) {
+                LOG("sdl quit");
+                running = false;
+            }
+        }
 
-    SDL_Delay(5000);
+        SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0x0, 0xFF));
+
+        SDL_UpdateWindowSurface(window);
+    }
+
+    SDL_Delay(2000);
+
+    SDL_FreeSurface(surface);
 
     SDL_DestroyWindow(window);
 
---
+++ sdl2/light2d/run_debug.bat
@@ -0,0 +1,6 @@
+
+set PATH=%PATH%;D:\dev\sdk\sdl2\SDL2-2.0.14-win32-x64
+
+.\build\Debug\light2d.exe
+
+choice
